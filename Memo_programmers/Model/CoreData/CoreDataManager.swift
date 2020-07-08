3c3c466c164481130afafc14a34eea7b075590d6
Haeuncs <haeun.developer@gmail.com>
2020-07-08 23:03:19 +0900

    feat: Add unit test case

--- Memo_programmers/Model/CoreData/CoreDataManager.swift
+++ Memo_programmers/Model/CoreData/CoreDataManager.swift
@@ -11,171 +11,203 @@ import UIKit
 
 class CoreDataManager {
 
-  static let sharedManager = CoreDataManager()
-  private init() {}
+    static let sharedManager = CoreDataManager()
+    private init() {}
 
-  lazy var persistentContainer: NSPersistentContainer = {
+    lazy var persistentContainer: NSPersistentContainer = {
 
-    let container = NSPersistentContainer(name: "Memo_programmers")
-    container.loadPersistentStores(completionHandler: { (_, error) in
+        let container = NSPersistentContainer(name: "Memo_programmers")
+        container.loadPersistentStores(completionHandler: { (_, error) in
 
-      if let error = error as NSError? {
-        fatalError("Unresolved error \(error), \(error.userInfo)")
-      }
-    })
-    return container
-  }()
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
 
-  func saveContext () {
-    let context = CoreDataManager.sharedManager.persistentContainer.viewContext
-    if context.hasChanges {
-      do {
-        try context.save()
-      } catch {
-        let nserror = error as NSError
-        fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-      }
-    }
-  }
-
-  // Insert
-  func add(newMemo: MemoData) -> (Bool, Error?) {
-    let managedContext = CoreDataManager.sharedManager.persistentContainer.viewContext
-
-    var coredataTypeArr: [Images] = []
-    for image in newMemo.imageArray ?? [] {
-      if let imageData = (NSEntityDescription.insertNewObject(forEntityName: "Images",
-                                                              into: managedContext) as? Images) {
-        imageData.identifier = image.image.jpegData(compressionQuality: 0.8)!
-        imageData.date = image.date
-        coredataTypeArr.append(imageData)
-      }
-    }
-
-    let memo = Memo(context: managedContext)
-    memo.title = newMemo.title
-    memo.memo = newMemo.memo
-    memo.date = newMemo.date
-    memo.identifier = newMemo.identifier
-    memo.images = NSSet(array: coredataTypeArr)
-
-    do {
-      try managedContext.save()
-      return (true, nil)
-    } catch let error as NSError {
-      print("Could not save. \(error), \(error.userInfo)")
-      return (false, error)
-    }
-  }
-
-  // delete
-  func delete(identifier: UUID) -> (Bool, Error?) {
-
-    let managedContext = CoreDataManager.sharedManager.persistentContainer.viewContext
-
-    let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Memo")
-    fetchRequest.returnsObjectsAsFaults = false
-    let predicate = NSPredicate(format: "identifier == %@", identifier as CVarArg)
-    fetchRequest.predicate = predicate
-
-    do {
-
-      let fetchResults: Array = try managedContext.fetch(fetchRequest)
-
-      for fetchResult in fetchResults {
-        if let managedObject = fetchResult as? NSManagedObject {
-
-          managedContext.delete(managedObject)
+    func saveContext () {
+        let context = CoreDataManager.sharedManager.persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
         }
-      }
-    } catch let error as NSError {
-      print(error)
-      return (false, error)
     }
 
-    do {
-      try managedContext.save()
-      return (true, nil)
-    } catch let error as NSError {
-      return (false, error)
-    }
-  }
+    // Insert
+    func add(newMemo: MemoData) -> (Bool, Error?) {
+        let managedContext = CoreDataManager.sharedManager.persistentContainer.viewContext
 
-  // update
-  func update(updateMemo: MemoData) -> (Bool, Error?) {
-    let managedContext = CoreDataManager.sharedManager.persistentContainer.viewContext
-    let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Memo")
-    fetchRequest.returnsObjectsAsFaults = false
-    let predicate = NSPredicate(format: "identifier == %@", updateMemo.identifier! as CVarArg)
-    fetchRequest.predicate = predicate
-    fetchRequest.fetchLimit = 1
-    var coredataTypeArr: [Images] = []
-    for image in updateMemo.imageArray ?? [] {
-      if let imageData = (NSEntityDescription.insertNewObject(forEntityName: "Images",
-                                                              into: managedContext) as? Images) {
-        imageData.identifier = image.image.jpegData(compressionQuality: 0.8)!
-        imageData.date = image.date
-        coredataTypeArr.append(imageData)
-      }
-    }
-    do {
-      let fetchResult = try managedContext.fetch(fetchRequest).first
-      if let managedObject = fetchResult as? NSManagedObject {
-        managedObject.setValue(updateMemo.title, forKey: "title")
-        managedObject.setValue(updateMemo.memo, forKey: "memo")
-        managedObject.setValue(updateMemo.date, forKey: "date")
-        managedObject.setValue(updateMemo.modifyDate, forKey: "modifyDate")
-        managedObject.setValue(NSSet(array: coredataTypeArr), forKey: "images")
-      }
-      do {
-        try managedContext.save()
-        return (true, nil)
-      } catch let error as NSError {
-        return (false, error)
-      }
-    } catch let error as NSError {
-      return (false, error)
-    }
-  }
+        var coredataTypeArr: [Images] = []
+        for image in newMemo.imageArray ?? [] {
+            if let imageData = (NSEntityDescription.insertNewObject(forEntityName: "Images",
+                                                                    into: managedContext) as? Images) {
+                imageData.identifier = image.image.jpegData(compressionQuality: 0.8)!
+                imageData.date = image.date
+                coredataTypeArr.append(imageData)
+            }
+        }
 
-  // fetch all memo
-  func fetchAllMemos() -> [Memo]? {
-    let managedContext = CoreDataManager.sharedManager.persistentContainer.viewContext
-    let fetchRequest2 = NSFetchRequest<Memo>(entityName: "Memo")
-    // unit test check
-    if NSClassFromString("XCTest") != nil {
-      fetchRequest2.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
-    } else {
-      let orderType = userPreferences.getOrderType()
-      switch orderType {
-      case .title:
-        fetchRequest2.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true,
-                                                          selector: #selector(NSString.localizedCaseInsensitiveCompare))]
-      case .createDate:
-        fetchRequest2.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
-      case .modifyDate:
-        fetchRequest2.sortDescriptors = [NSSortDescriptor(key: "modifyDate", ascending: false)]
-      }
-    }
-    do {
-      let memos = try managedContext.fetch(fetchRequest2)
-      return memos
-    } catch let error as NSError {
-      print("Could not fetch. \(error), \(error.userInfo)")
-      return nil
+        let memo = Memo(context: managedContext)
+        memo.title = newMemo.title
+        memo.memo = newMemo.memo
+        memo.date = newMemo.date
+        memo.identifier = newMemo.identifier
+        memo.images = NSSet(array: coredataTypeArr)
+
+        do {
+            try managedContext.save()
+            return (true, nil)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+            return (false, error)
+        }
     }
 
-  }
+    // delete
+    func delete(identifier: UUID) -> (Bool, Error?) {
 
-  // flush
-  func flushData() {
+        let managedContext = CoreDataManager.sharedManager.persistentContainer.viewContext
 
-    let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest<NSFetchRequestResult>(entityName: "Memo")
-    if let objs = try? CoreDataManager.sharedManager.persistentContainer.viewContext.fetch(fetchRequest) {
-      for case let obj as NSManagedObject in objs {
-        CoreDataManager.sharedManager.persistentContainer.viewContext.delete(obj)
-      }
-      try? CoreDataManager.sharedManager.persistentContainer.viewContext.save()
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Memo")
+        fetchRequest.returnsObjectsAsFaults = false
+        let predicate = NSPredicate(format: "identifier == %@", identifier as CVarArg)
+        fetchRequest.predicate = predicate
+
+        do {
+
+            let fetchResults: Array = try managedContext.fetch(fetchRequest)
+
+            for fetchResult in fetchResults {
+                if let managedObject = fetchResult as? NSManagedObject {
+
+                    managedContext.delete(managedObject)
+                }
+            }
+        } catch let error as NSError {
+            print(error)
+            return (false, error)
+        }
+
+        do {
+            try managedContext.save()
+            return (true, nil)
+        } catch let error as NSError {
+            return (false, error)
+        }
     }
-  }
+
+    // update
+    func update(updateMemo: MemoData) -> (Bool, Error?) {
+        let managedContext = CoreDataManager.sharedManager.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Memo")
+        fetchRequest.returnsObjectsAsFaults = false
+        let predicate = NSPredicate(format: "identifier == %@", updateMemo.identifier! as CVarArg)
+        fetchRequest.predicate = predicate
+        fetchRequest.fetchLimit = 1
+        var coredataTypeArr: [Images] = []
+        for image in updateMemo.imageArray ?? [] {
+            if let imageData = (NSEntityDescription.insertNewObject(forEntityName: "Images",
+                                                                    into: managedContext) as? Images) {
+                imageData.identifier = image.image.jpegData(compressionQuality: 0.8)!
+                imageData.date = image.date
+                coredataTypeArr.append(imageData)
+            }
+        }
+        do {
+            let fetchResult = try managedContext.fetch(fetchRequest).first
+            if let managedObject = fetchResult as? NSManagedObject {
+                managedObject.setValue(updateMemo.title, forKey: "title")
+                managedObject.setValue(updateMemo.memo, forKey: "memo")
+                managedObject.setValue(updateMemo.date, forKey: "date")
+                managedObject.setValue(updateMemo.modifyDate, forKey: "modifyDate")
+                managedObject.setValue(NSSet(array: coredataTypeArr), forKey: "images")
+            }
+            do {
+                try managedContext.save()
+                return (true, nil)
+            } catch let error as NSError {
+                return (false, error)
+            }
+        } catch let error as NSError {
+            return (false, error)
+        }
+    }
+
+    // fetch all memo
+    func fetchAllMemos() -> [Memo]? {
+        let managedContext = CoreDataManager.sharedManager.persistentContainer.viewContext
+        let fetchRequest2 = NSFetchRequest<Memo>(entityName: "Memo")
+        // unit test check
+        if NSClassFromString("XCTest") != nil {
+            fetchRequest2.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
+        } else {
+            let orderType = userPreferences.getOrderType()
+            switch orderType {
+            case .title:
+                fetchRequest2.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true,
+                                                                  selector: #selector(NSString.localizedCaseInsensitiveCompare))]
+            case .createDate:
+                fetchRequest2.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
+            case .modifyDate:
+                fetchRequest2.sortDescriptors = [NSSortDescriptor(key: "modifyDate", ascending: false)]
+            }
+        }
+        do {
+            let memos = try managedContext.fetch(fetchRequest2)
+            return memos
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+            return nil
+        }
+
+    }
+
+    // flush
+    func flushData() {
+
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest<NSFetchRequestResult>(entityName: "Memo")
+        if let objs = try? CoreDataManager.sharedManager.persistentContainer.viewContext.fetch(fetchRequest) {
+            for case let obj as NSManagedObject in objs {
+                CoreDataManager.sharedManager.persistentContainer.viewContext.delete(obj)
+            }
+            try? CoreDataManager.sharedManager.persistentContainer.viewContext.save()
+        }
+    }
+
+    func reset() {
+        let container = persistentContainer
+        let coordinator = container.persistentStoreCoordinator
+        if let store = coordinator.destroyPersistentStore(type: NSSQLiteStoreType) {
+            do {
+                try coordinator.addPersistentStore(ofType: NSSQLiteStoreType,
+                                                   configurationName: nil,
+                                                   at: store.url,
+                                                   options: nil)
+            } catch {
+                print(error)
+            }
+        }
+
+    }
+}
+
+extension NSPersistentStoreCoordinator {
+    func destroyPersistentStore(type: String) -> NSPersistentStore? {
+        guard
+            let store = persistentStores.first(where: { $0.type == type }),
+            let storeURL = store.url
+            else {
+                return nil
+        }
+
+        try? destroyPersistentStore(at: storeURL, ofType: store.type, options: nil)
+
+        return store
+    }
+
 }
--- Memo_programmersTests/Memo_programmersTests.swift
+++ Memo_programmersTests/Memo_programmersTests.swift
@@ -15,83 +15,112 @@ import CoreData
  */
 class Memo_programmersTests: XCTestCase {
 
-  var coreDataManager: CoreDataManager!
+    var coreDataManager: CoreDataManager!
 
-  override func setUp() {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-    coreDataManager = CoreDataManager.sharedManager
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        coreDataManager = CoreDataManager.sharedManager
 
-  }
-
-  override func tearDown() {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-  }
-
-  func testExample() {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-  }
-
-  func testPerformanceExample() {
-    // This is an example of a performance test case.
-    measure {
-      // Put the code you want to measure the time of here.
     }
-  }
 
-  func test_init_coreDataManager() {
-    let instance = CoreDataManager.sharedManager
-    XCTAssertNotNil( instance )
-  }
-  func test_coreDataStackInitialization() {
-    let coreDataStack = CoreDataManager.sharedManager.persistentContainer
-    XCTAssertNotNil( coreDataStack )
-  }
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
 
-  /**
-   create test
-   */
-  func test_create_mock_up_data() {
-    let countBeforeAdd = coreDataManager.fetchAllMemos()?.count ?? 0
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
 
-    let memo_1 = MemoData(title: "test1", memo: "memo1", date: Date(), identifier: UUID())
-    let memo_2 = MemoData(title: "test2", memo: "memo2", date: Date(), identifier: UUID())
-    let memo_3 = MemoData(title: "test3", memo: "memo3", date: Date(), identifier: UUID())
-    let memo_4 = MemoData(title: "test4", memo: "memo4", date: Date(), identifier: UUID())
-    XCTAssertNotNil(coreDataManager.add(newMemo: memo_1))
-    XCTAssertNotNil(coreDataManager.add(newMemo: memo_2))
-    XCTAssertNotNil(coreDataManager.add(newMemo: memo_3))
-    XCTAssertNotNil(coreDataManager.add(newMemo: memo_4))
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
 
-    XCTAssertEqual(coreDataManager.fetchAllMemos()?.count, countBeforeAdd + 4)
-  }
-  /**
-   delete test
-   */
-  func test_deleteMemo() {
-    let testUUID = UUID()
-    let testMemo = MemoData(title: "test", memo: "test", date: Date(), identifier: testUUID)
-    XCTAssertNotNil(coreDataManager.add(newMemo: testMemo))
+    func test_init_coreDataManager() {
+        let instance = CoreDataManager.sharedManager
+        XCTAssertNotNil( instance )
+    }
+    func test_coreDataStackInitialization() {
+        let coreDataStack = CoreDataManager.sharedManager.persistentContainer
+        XCTAssertNotNil( coreDataStack )
+    }
 
-    let countMemoAfterAdd = coreDataManager.fetchAllMemos()?.count ?? 0
+    /**
+     create test
+     */
+    func test_create_mock_up_data() {
+        let countBeforeAdd = coreDataManager.fetchAllMemos()?.count ?? 0
 
-    XCTAssertNotNil(coreDataManager.delete(identifier: testUUID))
+        let memo_1 = MemoData(title: "test1", memo: "memo1", date: Date(), identifier: UUID())
+        let memo_2 = MemoData(title: "test2", memo: "memo2", date: Date(), identifier: UUID())
+        let memo_3 = MemoData(title: "test3", memo: "memo3", date: Date(), identifier: UUID())
+        let memo_4 = MemoData(title: "test4", memo: "memo4", date: Date(), identifier: UUID())
+        XCTAssertNotNil(coreDataManager.add(newMemo: memo_1))
+        XCTAssertNotNil(coreDataManager.add(newMemo: memo_2))
+        XCTAssertNotNil(coreDataManager.add(newMemo: memo_3))
+        XCTAssertNotNil(coreDataManager.add(newMemo: memo_4))
 
-    XCTAssertEqual((coreDataManager.fetchAllMemos()?.count ?? 0), countMemoAfterAdd - 1)
-  }
-  /**
-   update test
-   */
-  func test_update() {
-    let firstMemo = coreDataManager.fetchAllMemos()?.first
+        XCTAssertEqual(coreDataManager.fetchAllMemos()?.count, countBeforeAdd + 4)
+    }
+    /**
+     delete test
+     */
+    func test_deleteMemo() {
+        let testUUID = UUID()
+        let testMemo = MemoData(title: "test", memo: "test", date: Date(), identifier: testUUID)
+        XCTAssertNotNil(coreDataManager.add(newMemo: testMemo))
 
-    let checkUpdateTitle = "update"
-    firstMemo?.title = checkUpdateTitle
-    XCTAssertNotNil(coreDataManager.update(updateMemo: MemoData(title: checkUpdateTitle, memo: firstMemo?.memo, date: firstMemo?.date, identifier: firstMemo?.identifier)))
+        let countMemoAfterAdd = coreDataManager.fetchAllMemos()?.count ?? 0
 
-    let firstMemoAfterUpdate = coreDataManager.fetchAllMemos()?.first
+        XCTAssertNotNil(coreDataManager.delete(identifier: testUUID))
 
-    XCTAssertEqual(firstMemoAfterUpdate?.title, checkUpdateTitle)
-  }
+        XCTAssertEqual((coreDataManager.fetchAllMemos()?.count ?? 0), countMemoAfterAdd - 1)
+    }
+    /**
+     update test
+     */
+    func test_update() {
+        let firstMemo = coreDataManager.fetchAllMemos()?.first
+
+        let checkUpdateTitle = "update"
+        firstMemo?.title = checkUpdateTitle
+        XCTAssertNotNil(coreDataManager.update(updateMemo: MemoData(title: checkUpdateTitle, memo: firstMemo?.memo, date: firstMemo?.date, identifier: firstMemo?.identifier)))
+
+        let firstMemoAfterUpdate = coreDataManager.fetchAllMemos()?.first
+
+        XCTAssertEqual(firstMemoAfterUpdate?.title, checkUpdateTitle)
+    }
+
+    func test_reset() {
+
+        coreDataManager.flushData()
+
+        XCTAssertEqual(coreDataManager.fetchAllMemos()?.count ?? 0, 0)
+
+        let memo_1 = MemoData(title: "test1", memo: "memo1", date: Date(), identifier: UUID())
+        let memo_2 = MemoData(title: "test2", memo: "memo2", date: Date(), identifier: UUID())
+        let memo_3 = MemoData(title: "test3", memo: "memo3", date: Date(), identifier: UUID())
+        let memo_4 = MemoData(title: "test4", memo: "memo4", date: Date(), identifier: UUID())
+        XCTAssertNotNil(coreDataManager.add(newMemo: memo_1))
+        XCTAssertNotNil(coreDataManager.add(newMemo: memo_2))
+        XCTAssertNotNil(coreDataManager.add(newMemo: memo_3))
+        XCTAssertNotNil(coreDataManager.add(newMemo: memo_4))
+
+        XCTAssertEqual(coreDataManager.fetchAllMemos()?.count ?? 0, 4)
+
+        coreDataManager.reset()
+
+        XCTAssertEqual(coreDataManager.fetchAllMemos()?.count ?? 0, 0)
+        let memo_5 = MemoData(title: "test5", memo: "memo4", date: Date(), identifier: UUID())
+
+        XCTAssertNotNil(coreDataManager.add(newMemo: memo_5))
+
+        XCTAssertEqual(coreDataManager.fetchAllMemos()?.count ?? 0, 1)
+
+    }
+
 }
 // swiftlint:enable all
